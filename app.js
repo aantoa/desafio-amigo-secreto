d9af3e0e30118125474de2c67b8bbcacf25fabc3
aanahia <anahi2654@gmail.com>
2025-02-04 12:06:37 -0500

    Implementacion de funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 let amigos = [];
+
+function agregarAmigo() {
+  const input = document.getElementById("amigo");
+  const nombre = input.value.trim();
+  if (nombre === "") {
+    alert("Por favor, ingrese un nombre válido.");
+    return;
+  }
+  if (amigos.includes(nombre)) {
+    alert("Este nombre ya ha sido ingresado.");
+    return;
+  }
+  amigos.push(nombre);
+  input.value = "";
+}
