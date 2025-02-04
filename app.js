6129832ac1f69ef5ec460ac961fa7a6e0677c331
aanahia <anahi2654@gmail.com>
2025-02-04 12:18:38 -0500

    Implementacion de funcion para actualizar lista de amigos

--- app.js
+++ app.js
@@ -4,13 +4,25 @@ function agregarAmigo() {
   const input = document.getElementById("amigo");
   const nombre = input.value.trim();
   if (nombre === "") {
-    alert("Por favor, ingrese un nombre válido.");
+    alert("Por favor, inserte un nombre.");
     return;
   }
   if (amigos.includes(nombre)) {
-    alert("Este nombre ya ha sido ingresado.");
+    alert("Este nombre ya ha sido ingresado. Por favor, ingrese otro.");
+    input.value = "";
     return;
   }
   amigos.push(nombre);
   input.value = "";
+  actualizarLista();
+}
+
+function actualizarLista() {
+  const lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = amigos[i];
+    lista.appendChild(li);
+  }
 }
